71a0f0ee448b8f94ddc296f478225a4aeaf79e32
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-09-06 17:32:59 +0300

    updated

--- Models/main.py
+++ Models/main.py
@@ -1,27 +1,70 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 import click
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from user import User  # Import the User model (update the import path as needed)
-from city import City  # Import the City model
-from Hotel import Hotel  # Import the Hotel model
-from Room_type import RoomType  # Import the Room Type model
 
 # Database Configuration
-DATABASE_URL = "sqlite:///hotel_booking.db"  # SQLite database (update as needed)
+DATABASE_URL = "sqlite:///hotel_booking.db"  # SQLite database
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
-@click.group()
-def cli():
-    """Hotel Booking CLI Application"""
-    pass
+# Define User Model
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    username = Column(String(100), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    role = Column(String(20), nullable=False)
 
-@cli.command()
+# Define Reservation Model
+class Reservation(Base):
+    __tablename__ = "reservations"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(DateTime, default=datetime.now)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    hotel_id = Column(Integer, ForeignKey("hotels.id"))
+    room_id = Column(Integer, ForeignKey("rooms.id"))
+
+# Define Hotel Model
+class Hotel(Base):
+    __tablename__ = "hotels"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(255))
+    rating = Column(Float)
+    city_id = Column(Integer, ForeignKey("cities.id"))
+    rooms = relationship("Room", back_populates="hotel")
+
+# Define Room Model
+class Room(Base):
+    __tablename__ = "rooms"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(255))
+    price = Column(Float)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"))
+    hotel = relationship("Hotel", back_populates="rooms")
+
+# Define City Model
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    hotels = relationship("Hotel", back_populates="city")
+
+# Create tables
+Base.metadata.create_all(engine)
+
+@click.command()
 @click.option("--name", prompt="User's Full Name", help="User's Full Name")
 @click.option("--username", prompt="Username", help="Username")
 @click.option("--email", prompt="Email", help="Email")
 @click.option("--password", prompt="Password", hide_input=True, help="Password")
-@click.option("--role", type=click.Choice(["user", "admin"]), help="User Role (user/admin)")
+@click.option("--role", type=click.Choice(["user", "admin"]), default="user", help="User Role (user/admin)")
 def add_user(name, username, email, password, role):
     """Add a new user to the database."""
     session = Session()
@@ -31,10 +74,10 @@ def add_user(name, username, email, password, role):
     session.close()
     print("User added successfully!")
 
-@cli.command()
-@click.option("--name", prompt="City Name", help="City Name")
+@click.command()
+@click.option("--name", prompt="City Name", help="City's Name")
 def add_city(name):
-    """Add a new city to the database."""
+    """Add a new city."""
     session = Session()
     city = City(name=name)
     session.add(city)
@@ -42,56 +85,66 @@ def add_city(name):
     session.close()
     print("City added successfully!")
 
-@cli.command()
-@click.option("--name", prompt="Hotel Name", help="Hotel Name")
-@click.option("--city", prompt="City Name", help="City Name")
-@click.option("--room-types", prompt="Room Types (comma-separated)", help="Room Types")
-def add_hotel(name, city, room_types):
-    """Add a new hotel to the database."""
+def print_user_details(username):
     session = Session()
-    city_obj = session.query(City).filter_by(name=city).first()
-    if city_obj:
-        room_type_names = [rt.strip() for rt in room_types.split(",")]
-        hotel = Hotel(name=name, city_id=city_obj.id)
-        for room_type_name in room_type_names:
-            room_type = session.query(RoomType).filter_by(name=room_type_name).first()
-            if room_type:
-                hotel.room_types.append(room_type)
-        session.add(hotel)
-        session.commit()
-        session.close()
-        print("Hotel added successfully!")
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        print(f"User ID: {user.id}")
+        print(f"Name: {user.name}")
+        print(f"Email: {user.email}")
+        print(f"Role: {user.role}")
     else:
-        print(f"City '{city}' not found.")
-
-@cli.command()
-@click.option("--name", prompt="Room Type Name", help="Room Type Name")
-def add_room_type(name):
-    """Add a new room type to the database."""
-    session = Session()
-    room_type = RoomType(name=name)
-    session.add(room_type)
-    session.commit()
+        print(f"User with username '{username}' not found.")
     session.close()
-    print("Room Type added successfully!")
 
-@cli.command()
-@click.option("--city", prompt="City Name", help="City Name")
-def search_hotels(city):
-    """Search for hotels in a city."""
+
+
+
+def print_city_reservations(city_name):
     session = Session()
-    city_obj = session.query(City).filter_by(name=city).first()
-    if city_obj:
-        hotels = session.query(Hotel).filter_by(city_id=city_obj.id).all()
-        session.close()
-        if hotels:
-            print(f"Hotels in {city}:")
-            for hotel in hotels:
-                print(f"- {hotel.name}")
-        else:
-            print(f"No hotels found in {city}.")
+    city = session.query(City).filter_by(name=city_name).first()
+    if city:
+        print(f"City: {city.name}")
+        hotels = city.hotels
+        for hotel in hotels:
+            print(f"Hotel: {hotel.name}")
+            reservations = hotel.reservations
+            for reservation in reservations:
+                print(f"Reservation Date: {reservation.date}")
+                
     else:
-        print(f"City '{city}' not found.")
+        print(f"City '{city_name}' not found.")
+    session.close()
+
+def print_room_types(hotel_name):
+    session = Session()
+    hotel = session.query(Hotel).filter_by(name=hotel_name).first()
+    if hotel:
+        print(f"Hotel: {hotel.name}")
+        rooms = hotel.rooms
+        for room in rooms:
+            print(f"Room Type: {room.name}")
+            print(f"Description: {room.description}")
+            print(f"Price: {room.price}")
+        
+    else:
+        print(f"Hotel '{hotel_name}' not found.")
+    session.close()
 
 if __name__ == "__main__":
-    cli()
+    choice = input("What do you want to do (add_user/add_city/print_user/print_city/print_room)? ").strip().lower()
+    if choice == "add_user":
+        add_user()
+    elif choice == "add_city":
+        add_city()
+    elif choice == "print_user":
+        username = input("Enter the username: ").strip()
+        print_user_details(username)
+    elif choice == "print_city":
+        city_name = input("Enter the city name: ").strip()
+        print_city_reservations(city_name)
+    elif choice == "print_room":
+        hotel_name = input("Enter the hotel name: ").strip()
+        print_room_types(hotel_name)
+    else:
+        print("Invalid choice. Please choose 'add_user', 'add_city', 'print_user', 'print_city', or 'print_room'.")
